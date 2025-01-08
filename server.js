9c68647b8d10402f663f18216ca745d374cc114e
yvonnegichovi <yvonnegichovi@gmail.com>
2025-01-08 12:37:33 +0300

    Feat: Removed MongoDB from the project since it will not be in use

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -9,18 +8,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true;
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error(err));
-
+// Simple route
 app.get('/', (req, res) => {
     res.send('Portfolio Backend is Running');
 });
 
+// Serve static files for React build
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Server listening
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
